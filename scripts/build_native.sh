68aebe2c39e4f9e7eb125dc190d71402b899a3e1
Qijia Liu <liumeo@pku.edu.cn>
2022-11-03 21:04:09 -0400

    asan doesn't work on macOS

--- scripts/build_native.sh
+++ scripts/build_native.sh
@@ -6,12 +6,15 @@ make clean
 make
 mv magic/magic.mgc ../wasm
 
-cd ../test
-ln -s ../wasm/magic.mgc
-lib_path=../file/src/.libs
-gcc \
-  -fsanitize=address \
-  -o test_sanitizer \
-  ../wasm/api.c test_sanitizer.c \
-  -L $lib_path -l magic
-LD_LIBRARY_PATH=$lib_path ./test_sanitizer
+os=`uname`
+if [[ $os == 'Linux' ]]; then
+  cd ../test
+  [[ -f magic.mgc ]] || ln -s ../wasm/magic.mgc
+  lib_path=../file/src/.libs
+  gcc \
+    -fsanitize=address \
+    -o test_sanitizer \
+    ../wasm/api.c test_sanitizer.c \
+    -L $lib_path -l magic
+  LD_LIBRARY_PATH=$lib_path ./test_sanitizer
+fi
