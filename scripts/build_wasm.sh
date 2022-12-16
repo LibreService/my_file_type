871fcea8704e9c3679ff711d4d9a22feda8e1179
Qijia Liu <liumeo@pku.edu.cn>
2022-12-16 18:52:27 -0500

    increase stack size due to emscripten 3.1.27

--- scripts/build_wasm.sh
+++ scripts/build_wasm.sh
@@ -2,6 +2,7 @@ set -e
 cd wasm
 emcc \
   -O2 \
+  -s STACK_SIZE=5MB \
   -s ALLOW_MEMORY_GROWTH=1 \
   -s EXPORTED_FUNCTIONS=_init,_get_type,_get_mime,_get_extension \
   -s EXPORTED_RUNTIME_METHODS='["ccall"]' \
